9b02536142ec24c3e47f3fd3bfd46b2229966606
Jackson Lucchesi <jacksonlucchesi@RAYMONDs-MacBook-Pro.local>
2019-11-13 10:44:14 -0700

    Added ArrayPractice project

---
+++ Workspace2/ArrayPractice/src/ArrayPractice.java
@@ -0,0 +1,11 @@
+
+public class ArrayPractice {
+	static String[] school = {"Prospect", "Ridge", "Academy"};
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < school.length; i++) {
+			System.out.print(school[i] + " ");
+		}
+	}
+
+}
